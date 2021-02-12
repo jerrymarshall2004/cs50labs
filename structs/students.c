a5423ab8673101867c5445374f6c45f53793c3bc
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-02-12 10:32:42 -0500

    Update and rename mystruct.c to students.c

---
+++ structs/students.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cs50.h>
+//////////////////////////////////////////////////////////////////////////////////////////////////
+typedef struct   //This outlines a custom data type, a structure, sometimes called an object in other languages
+{
+    
+//write the code to define fields in the structure for: name, studentNum, grade, courseAverage
+    
+}
+student;  //It is common to forget the ; after the structure name, watch out for this
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+int main(void)
+{    
+    //prompt user for number of students, store this in a variable
+    
+    
+    // declare a new variable using your new data type.  We defined it above, but havn't allocated any memory for it.
+    // You need to declare a variable to hold the data from the structure, use an array
+    
+    
+
+ 
+    
+    // add data to your new structure using a for loop, iterating for the number of students
+    
+    
+    // print out the data in your structure.  It should have a format similar to below
+    // Name: Michalenko
+    // Grade: 12
+    // Student #: 1234
+    // Average: 100
+    
+    
+}
