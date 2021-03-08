b701173f1dfa65a1f553ba83e306d183cf12d71a
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-03-08 15:50:57 -0500

    Update toupper.c

--- pointers/toupper.c
+++ pointers/toupper.c
@@ -1,1 +1,16 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
 
+int main(void)
+{
+    string s = get_string("s: ");
+
+    string t = s;
+
+    t[0] = toupper(t[0]);
+
+    // Print string twice
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+}
