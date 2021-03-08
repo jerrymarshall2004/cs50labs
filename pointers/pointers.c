f53bd4d7e0e181b43ef06476ef5db3e3c59db7dc
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2021-03-08 16:59:44 -0500

    Update pointers.c

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,1 +1,16 @@
-# include<stio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string s = get_string("s: ");
+////modify the code to print string s in lower case, and string t with the first letter capitalized.
+    string t = s;
+
+    t[0] = toupper(t[0]);
+
+    // Print string twice
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+}
